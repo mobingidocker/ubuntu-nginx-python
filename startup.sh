5ee4d657d97ef54b83592a599851324c03e093eb
sawanoboly <sawanoboriyu@higanworks.com>
2016-03-14 17:15:50 +0900

    cherry-picked 2fe2bc9ed334d7fffc32f546cc3accc13693c730

--- startup.sh
+++ startup.sh
@@ -9,9 +9,14 @@ mkdir -p /srv/django
 cp -r /srv/code /srv/django/app
 chown -R www-data:www-data /srv/django/app
 
-pushd /srv/django/app
-	pip install -r requirements.txt
-popd
+echo "Running Pip..."
+cd /srv/django/app
+pip install -r requirements.txt | tee /var/log/pip.log
+
+if [ -f manage.py ]; then
+  echo "Migrate database"
+  python manage.py migrate | tee /var/log/migrate.log
+fi
 
 echo "complete" > /var/log/container_status
 
