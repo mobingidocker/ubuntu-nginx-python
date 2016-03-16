80fcd0cc6bb73cc557f69d1451945d4fd6e136e5
sawanoboly <sawanoboriyu@higanworks.com>
2016-03-17 02:40:45 +0900

    clean up

--- provision/pytnon27.sh
+++
@@ -1,8 +0,0 @@
-curl -sL https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | PYENV_DEBUG=true bash
-
-PYENV_PYTHON=2.7.11
-
-/opt/pyenv/bin/pyenv install ${PYENV_PYTHON}
-/opt/pyenv/bin/pyenv global  ${PYENV_PYTHON}
-/opt/pyenv/shims/pip install --upgrade pip
-/opt/pyenv/shims/pip install uwsgi
--- provision/pytnon34.sh
+++
@@ -1,3 +0,0 @@
-apt-get install -y python3 python3-pip python3-dev python3-setuptools uwsgi-plugin-python3
-pip3 install uwsgi
-
