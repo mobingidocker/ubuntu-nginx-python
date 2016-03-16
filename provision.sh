1a965649db9a632e3c592b32f30c6ba9a98fb9a9
sawanoboly <sawanoboriyu@higanworks.com>
2016-03-17 03:19:21 +0900

    3.4.4

--- provision.sh
+++ provision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-PYENV_PYTHON=3.5.1
+PYENV_PYTHON=3.4.4
 
 curl -sL https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | PYENV_DEBUG=true bash
 /opt/pyenv/bin/pyenv install ${PYENV_PYTHON}
